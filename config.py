57460861b35ca969298a64d1d0f171d0d01345e9
sachinopboy2 <sachin25u7@gmail.com>
2025-10-09 10:02:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sachinopboy2/Aahi-music",
+    "https://github.com/sachinopboy2/nayana2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
